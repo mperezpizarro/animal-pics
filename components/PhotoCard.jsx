42172c7e6f779d2ba91e062ddd836a7f41dbd634
mperezpizarro <marioperezpizarro@gmail.com>
2022-12-21 13:32:14 +0100

    noreferrer

--- components/PhotoCard.jsx
+++ components/PhotoCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 function PhotoCard({src}) {
   return (
     <div className="w-full max-w-3xl aspect-[16/10] mx-auto relative">
-        <a href={src} target="_blank">
+        <a href={src} target="_blank" rel="noreferrer">
           <Image src={src} fill alt="Foto de perro" className="object-cover object-center rounded-lg" />
         </a>
     </div>
