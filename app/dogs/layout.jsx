b3df445e637f750ec04ba5ee48c4a5ee82fec944
mperezpizarro <marioperezpizarro@gmail.com>
2022-12-21 13:23:59 +0100

    Credited APIs

--- app/dogs/layout.jsx
+++ app/dogs/layout.jsx
@@ -5,6 +5,9 @@ async function Layout({children}) {
     return (
         <main className="w-full max-w-4xl mx-auto py-2 px-4">
             <h2 className="font-bold text-xl md:text-3xl text-center mb-4">Fotos de shibas</h2>
+            <div className="text-center">
+                <small>Powered by <a className="underline" href="https://shibe.online/">Shibe Online API</a></small>
+            </div>
             {children}
             <div className="my-6 flex justify-center gap-x-4">
                 <Link href="/" className="text-white font-bold bg-amber-500 py-4 px-6 rounded-xl">
--- app/ducks/layout.jsx
+++ app/ducks/layout.jsx
@@ -5,6 +5,9 @@ async function Layout({children}) {
     return (
         <main className="w-full max-w-4xl mx-auto py-2 px-4">
             <h2 className="font-bold text-xl md:text-3xl text-center mb-4">Fotos de patos</h2>
+            <div className="text-center">
+                <small>Powered by <a className="underline" href="https://random-d.uk">random-d.uk API</a></small>
+            </div>
             {children}
             <div className="my-6 flex justify-center gap-x-4">
                 <Link href="/" className="text-white font-bold bg-cyan-600 py-4 px-6 rounded-xl">
