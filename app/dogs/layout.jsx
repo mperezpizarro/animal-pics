d779d860a288ba76f422dbaa9760573e83ceb2c1
mperezpizarro <marioperezpizarro@gmail.com>
2022-12-21 13:08:38 +0100

    Changed layout title

--- app/dogs/layout.jsx
+++ app/dogs/layout.jsx
@@ -4,7 +4,7 @@ import PicLinks from "../../components/PicLinks"
 async function Layout({children}) {
     return (
         <main className="w-full max-w-4xl mx-auto py-2 px-4">
-            <h2 className="font-bold text-xl md:text-3xl text-center mb-4">¡Alegra tu día con un bonito shiba!</h2>
+            <h2 className="font-bold text-xl md:text-3xl text-center mb-4">Fotos de shibas</h2>
             {children}
             <div className="my-6 flex justify-center gap-x-4">
                 <Link href="/" className="text-white font-bold bg-amber-500 py-4 px-6 rounded-xl">
