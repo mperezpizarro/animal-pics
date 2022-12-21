78fe047c733529af73f3391e7697e5d5233e1632
mperezpizarro <marioperezpizarro@gmail.com>
2022-12-21 09:55:39 +0100

    Changed animal list

--- app/head.jsx
+++ app/head.jsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Animal Pics</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Descubre fotos adorables de perritos, patitos y gatitos." />
+      <meta name="description" content="Descubre fotos adorables de shibas, patos y zorros." />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="text-center">
       <h1 className="text-3xl font-bold">ANIMALS PICS</h1>
-      <h2 className="text-xl font-semibold">Descubre fotos adorables de shibas, patitos y gatitos</h2>
+      <h2 className="text-xl font-semibold">Descubre fotos adorables de shibas, patos y zorros</h2>
 
       <section className="mt-6 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-2">
         <LinkCard href="/dogs">
@@ -13,8 +13,8 @@ export default function Home() {
         <LinkCard href="/ducks">
           Ver fotos de patos
         </LinkCard>
-        <LinkCard href="/cats">
-          Ver fotos de gatos
+        <LinkCard href="/foxes">
+          Ver fotos de zorros
         </LinkCard>
       </section>
     </main>
