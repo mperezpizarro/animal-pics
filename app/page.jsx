cba816d824d7126c27ca27e362e0ff5cb2487c00
mperezpizarro <marioperezpizarro@gmail.com>
2022-12-22 08:58:49 +0100

    Changed padding

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,12 @@ import LinkCard from "../components/LinkCard"
 export default function Home() {
   return (
     <main className="text-center">
-      <h1 className="text-3xl font-bold">ANIMALS PICS</h1>
-      <h2 className="text-xl font-semibold">Descubre fotos adorables de shibas, patos y zorros</h2>
+      <section className="py-2 px-4">
+        <h1 className="text-3xl font-bold">ANIMALS PICS</h1>
+        <h2 className="text-xl font-semibold">Descubre fotos adorables de shibas, patos y zorros</h2>
+      </section>
 
-      <section className="mt-6 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-2">
+      <section className="mt-6 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-2 px-4">
         <LinkCard href="/dogs">
           Ver fotos de shibas
         </LinkCard>
